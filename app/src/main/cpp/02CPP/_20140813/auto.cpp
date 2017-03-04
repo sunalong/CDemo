e7f28b488409237c598e8ab164882e65db3ee2f5
sunalong <sunalong@ztgame.com>
2017-03-04 23:07:37 +0800

    自动类型转化

---
+++ app/src/main/cpp/02CPP/_20140813/auto.cpp
@@ -0,0 +1,13 @@
+//
+// Created by sunalong on 17/3/4 15:39.
+// Email:466210864@qq.com
+//
+#include <iostream>
+using namespace std;
+int main(){
+    auto num = 10.8;//自动变量，自动匹配类型
+    auto numA = 10;
+    auto nStr = "this is string";
+    cout<<"num:"<<num<<" numA:"<<numA<<" nStr:"<<nStr<<endl;
+    return 0;
+}
