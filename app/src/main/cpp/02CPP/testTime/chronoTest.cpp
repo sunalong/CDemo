9e2c72b9c1e7fe3e10db986ba466d0a8bc4ed23a
sunalong <sunalong@ztgame.com>
2017-03-08 20:17:58 +0800

    纳秒级计算

---
+++ app/src/main/cpp/02CPP/testTime/chronoTest.cpp
@@ -0,0 +1,29 @@
+//
+// Created by sunalong on 17/3/8 20:09.
+// Email:466210864@qq.com
+//
+// steady_clock example
+#include <iostream>
+#include <ctime>
+#include <ratio>
+#include <chrono>
+
+int main() {
+    using namespace std::chrono;
+
+    steady_clock::time_point t1 = steady_clock::now();
+//    std::cout << "1,t1:" << t1.count() << std::endl;
+    std::cout << "printing out 1000 stars...\n";
+    for (int i = 0; i < 1000; ++i) std::cout << "*";
+    std::cout << std::endl;
+
+    steady_clock::time_point t2 = steady_clock::now();
+
+    duration<double> time_span = duration_cast<duration<double>>(t2 - t1);
+
+    std::cout << "It took me " << time_span.count() << " seconds.";
+    std::cout << std::endl;
+//    It took me 0.000151389 seconds.
+    return 0;
+}
+
